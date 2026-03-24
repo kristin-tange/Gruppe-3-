c63b900ace71c399a08681130d626296b726d49c
Kristin Tange <kristintange@gmail.com>
2026-03-24 20:57:31 +0100

    fetch and show data from api/meetup in hero and event-description

--- Gatherly/src/js/singleEvent.js
+++ Gatherly/src/js/singleEvent.js
@@ -5,33 +5,103 @@
 const BASE_URL = "http://localhost:3000/api";
 const API_KEY = "12345";
 let users = [];
-let posts = [];
-const eventId = 1;
-let postId = null;
-let commentId = null;
-
-const eventTitle = document.getElementById("event-title");
-const eventDate = document.getElementById("event-date");
-const eventPlace = document.getElementById("event-place");
-const eventTime = document.getElementById("event-time");
-const eventPrice = document.getElementById("event-price");
-const category = document.getElementById("category-tag");
-const eventContainer = document.getElementById("event-container");
+// let posts = [];
+// const eventId = 1;
+// let postId = null;
+// let commentId = null;
 
 async function fetchUsers() {
   const response = await fetch(`${BASE_URL}/users`);
   users = await response.json();
+  console.log(users);
   return users;
+}
+// TODO: function getUserName (innlegg og kommentarer)
+function getUserName(userId) {
+  const user = users.find((u) => u.id == userId);
+  return user ? user.name : "Ukjent forfatter";
 }
 
 async function fetchSingleEvent() {
-  const params = new URLSearchParams(window.location.search);
-  const eventId = params.get("id");
-  const response = await fetch(`${BASE_URL}/meetups/${eventId}`);
+  // const params = new URLSearchParams(window.location.search);
+  // const eventId = params.get("id");
+  const response = await fetch(`${BASE_URL}/meetups/1`);
   const event = await response.json();
-
   console.log(event);
-  // showSingleEvent(event);
+  showSingleEvent(event);
+}
+
+// VISE EVENTER/MEETUPS
+
+function formatDate(data) {
+  const date = new Date(data);
+  return date.toLocaleDateString("no-NO", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
+
+function formatTime(data) {
+  const time = new Date(data);
+  return time.toLocaleTimeString("no-NO", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+}
+
+function showSingleEvent(event) {
+  const heroContainer = document.getElementById("hero-container");
+  const descriptionContainer = document.getElementById("description-container");
+  const formattedDate = formatDate(event.date);
+  const formattedTime = formatTime(event.date);
+  const formattedCreatedDate = formatDate(event.created);
+  const formattedCreatedTime = formatTime(event.created);
+  const formattedUpdatedDate = formatDate(event.updated);
+  const formattedUpdatedTime = formatTime(event.updated);
+  const tags = event.tags;
+  const formattedTags = tags.join(", ");
+
+  document.title = `${event.name}`;
+
+  heroContainer.innerHTML = `<section role="img" class="event-hero"  alt="${event.imageAlt}" style="background: linear-gradient(rgba(207, 207, 207, 0.55),rgba(131, 131, 131, 0.55)), url('${event.image}')center / cover no-repeat;">
+      <div class="hero-overlay">
+        <h1 class="event-title">${event.name}</h1>
+        <p class="event-date hero-textbox">${formattedDate}</p>
+        <div class="event-info hero-textbox">
+          <p class="event-place">${event.location} /</p>
+          <p class="event-time">kl. ${formattedTime} /</p>
+          <p class="event-price">${event.tags[4]}</p>
+        </div>
+        <div id="filter${event.category}" class="category tag hero-tag">${event.category}</div>
+      </div>
+    </section>`;
+
+  descriptionContainer.innerHTML = `<section class="section-grid">
+  <div class="description-header">
+  <h2>Om arrangementet</h2>
+  <p class="tags">#${formattedTags}</p>
+  </div>
+  <p>
+  ${event.description}
+  </p>
+  <p class="muted margin-bottom">
+  Opprettet: <span id="created">${formattedCreatedDate} kl. ${formattedCreatedTime}</span> <br />Sist
+            oppdatert:
+            <span id="updated">${formattedUpdatedDate} kl. ${formattedUpdatedTime}</span>
+          </p>
+            <button class="btn btn-primary" id="sign-up-btn">Påmelding</button>
+        </section>`;
+
+  // MIDLERTIDIG "PÅMELDING"
+  const signUpBtn = document.getElementById("sign-up-btn");
+  signUpBtn.addEventListener("click", () => {
+    alert("Din påmelding er registrert.");
+  });
+
+  //TODO - nice to have: påmeldingsskjema
+  // TODO - nice to have: vise pop-up med informasjon fra påmelding
+  // TODO - nice to have: ikon og counter med antall påmeldte
 }
 
 // async function fetchPosts() {
@@ -40,25 +110,6 @@ async function fetchSingleEvent() {
 //   console.log(posts);
 // }
 
-// TODO: function getUserName (innlegg og kommentarer)
-
-// VISE EVENTER/MEETUPS
-// function showSingleEvent(event) {
-//   eventContainer.innerHTML = ;
-// }
-
-// MIDLERTIDIG "PÅMELDING"
-//TODO - nice to have: påmeldingsskjema
-// TODO - nice to have: vise pop-up med informasjon fra påmelding
-// TODO - nice to have: ikon og counter med antall påmeldte
-
-const signUpBtn = document.getElementById("sign-up-btn");
-
-signUpBtn.addEventListener("click", () => {
-  alert("Din påmelding er registrert.");
-});
-
-// TODO: oppdatere dokumenttittel ut fra hvilket event det er
 // ÅPNE OG LUKKE OVERLAY: INNLEGG
 const overlayBtn = document.getElementById("open-overlay-btn");
 const closeOverlayBtn = document.getElementById("close-btn");
@@ -147,7 +198,7 @@ function createPost() {
 async function init() {
   await fetchUsers();
   await fetchSingleEvent();
-  await fetchPosts();
+  // await fetchPosts();
 }
 
 init();
