a758969611484dd6774b921636a86aedfd2b70b5
oscarwirum <oscwir@gmail.com>
2026-03-24 14:25:06 +0100

    Fikset knapp i header for loggedIn eller ikke loggedIn. Endret noen hover animations i header og events page. Når man ikke er logget inn vil man kunne velge mellom å logge inn eller opprette bruker. Når man er logget inn kan man gå til sin bruker.

--- Gatherly/src/js/header.js
+++ Gatherly/src/js/header.js
@@ -2,14 +2,47 @@
 
 const header = document.querySelector("header");
 
+function isLoggedIn() {
+  //må gjøres om senere med API key
+  return localStorage.getItem("loggedIn") === "true";
+}
+
+function updateLoginArea() {
+  const loginArea = document.querySelector(".login-area");
+
+  if (!loginArea) return;
+
+  if (isLoggedIn()) {
+    loginArea.innerHTML = `
+      <a href="/Gatherly/profile.html" class="profile-btn">
+        <img src="/Gatherly/public/assets/icons/header-user.png" alt="Profil" />
+      </a>
+    `;
+  } else {
+    loginArea.innerHTML = `
+       <div class="login-wrapper">
+    
+      <img src="/Gatherly/public/assets/icons/login.png" alt="Logg inn" class="login-btn" />
+    
+
+    <div class="login-popup">
+      <a href="/Gatherly/login.html" class="popup-btn">Logg inn</a>
+      <a href="/Gatherly/register.html" class="popup-btn create">Opprett konto</a>
+    </div>
+  </div>
+
+    `;
+  }
+}
+
 function displayHeader() {
   header.innerHTML = `
     <div class="hamburger-backdrop"></div>
-      <button class="hamburger" aria-label="Åpne meny">
+      <div id="menu-div"><button class="hamburger" aria-label="Åpne meny">
         <div>
           <img src="/Gatherly/public/assets/icons/menu.png" alt="" />
         </div>
-      </button>
+      </button></div>
       <div id="logoCon">
         <a href="/Gatherly/index.html"
           ><img
@@ -18,11 +51,7 @@ function displayHeader() {
             id="topLogo"
         /></a>
       </div>
-      <button class="login" aria-label="Logg inn">
-        <div>
-        <a href="">
-        <img src="/Gatherly/public/assets/icons/login.png" alt="" /></a></div>
-      </button>
+      <div class="login-area" aria-label="Logg inn"></div>
       <nav class="sideNav">
         <ul>
           <li><a href="/Gatherly/index.html">Hjem</a></li>
@@ -30,6 +59,10 @@ function displayHeader() {
           <li><a href="">Kontakt</a></li>
         </ul>
       </nav>`;
+  updateLoginArea();
 }
+
+//midlertidig switch for loggedIn - Bytt mellom true eller false for å se endring på siden.
+localStorage.setItem("loggedIn", "false");
 
 displayHeader();
